cb8dd18c2adae3b47adf556b5b9f11f2ed4a0fb5
SamuelNLP <samuelnlpedro@gmail.com>
2018-09-13 01:34:18 +0100

    created aggregation tables for btc_usd_by_day_agg

--- postgreSQL_scripts/btc_usd_processing.sql
+++ postgreSQL_scripts/btc_usd_processing.sql
@@ -1,6 +1,7 @@
 -- create table that has ratio measures from prices and volumes
-DROP TABLE IF EXISTS btc_usd_by_hour_agg;
+DROP TABLE IF EXISTS btc_usd_by_hour_agg, btc_usd_by_day_agg;
 
+-- create by hour for a more expressive eda analysis
 CREATE TABLE btc_usd_by_hour_agg
 	AS ( 
 	-- define an intermediate selection to create lag columns
@@ -31,6 +32,37 @@ CREATE TABLE btc_usd_by_hour_agg
 		-- date as reference
 		date_
 	FROM btc_usd_by_hour
+	INNER JOIN lags USING(date_)
+	ORDER BY date_ DESC
+);
+
+-- create byu day for ML analysis
+CREATE TABLE btc_usd_by_day_agg
+	AS ( 
+	-- define an intermediate selection to create lag columns
+	WITH lags AS (
+		SELECT 
+			lag(weighted_price) OVER (ORDER BY date_) AS lag_weighted_price,
+			date_
+		FROM btc_usd
+	)
+	SELECT 
+		-- measures
+		("close" - "open") / "close" AS perc_close_open, "open", "close",
+		(high - low) / high AS perc_high_low, high, low,
+		(weighted_price - lag_weighted_price) / lag_weighted_price AS perc_weighted_price, weighted_price,
+		volume_btc, volume_currency,
+		-- possible aggregations
+		EXTRACT(dow FROM date_) AS weekday,
+		EXTRACT(DAY FROM date_) AS day_of_month,
+		EXTRACT(MONTH FROM date_) AS month_,
+		CASE WHEN EXTRACT(dow FROM date_) IN (0, 6) THEN 1
+			ELSE 0
+		END AS weekend,
+		EXTRACT(YEAR FROM date_) AS year_,
+		-- date as reference
+		date_
+	FROM btc_usd_by_day
 	INNER JOIN lags USING(date_)
 	ORDER BY date_ DESC
 );
