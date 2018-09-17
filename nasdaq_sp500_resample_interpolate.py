49bb03ee671c3188bc7f99e5d25d33c8615df5de
SamuelNLP <samuelnlpedro@gmail.com>
2018-09-17 02:21:23 +0100

    Correct interpolation in sp500 and nasdaq to avoing future data from leaking to past interpolated data. Ended up using a linear regression taking into account last 5 days.

--- nasdaq_sp500_resample_interpolate.py
+++ nasdaq_sp500_resample_interpolate.py
@@ -3,9 +3,46 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 import sqlalchemy
+from sklearn import linear_model
 from sqlalchemy import create_engine
 
 sns.set()
+
+
+# define regression model to fill weekend values
+def regression_with_past(col: pd.Series):
+    # find nans with valid values on right side
+    nan_left_idx = []
+
+    for idx_, value in enumerate(col):
+        if idx_ != len(col) - 1:
+            if (not np.isnan(col[idx_ + 1])) & (np.isnan(value)):
+                nan_left_idx.append(idx_)
+
+    # how many days to look back in regression
+    days_look_back = 5
+
+    # regression interactively
+    for idx_ in nan_left_idx:
+        # find data of training (non_nan) and test (nan)
+        if idx_ < days_look_back:
+            interval_use = list(range(idx_ + 1))
+            nan_idx = np.argwhere(np.isnan(col[interval_use]))
+            non_nan_idx = np.argwhere(col[interval_use].notnull())
+        else:
+            interval_use = list(range(idx_ - days_look_back, idx_ + 1))
+            nan_idx = np.argwhere(np.isnan(col[interval_use])) + idx_ - days_look_back
+            non_nan_idx = np.argwhere(col[interval_use].notnull()) + idx_ - days_look_back
+
+        # train model
+        regr = linear_model.LinearRegression()
+        regr.fit(non_nan_idx, col[non_nan_idx])
+
+        # fill values with results
+        col[nan_idx] = regr.predict(nan_idx)
+
+    return col
+
 
 # connection to database
 engine = create_engine('postgresql://postgres:postgres@localhost:5432/bitcoin_test')
@@ -16,7 +53,7 @@ con = engine.connect()
 tables = ['nasdaq_clean', 'sp500_clean']
 
 # check if I wnat a plot
-plotit = False
+plotit = True
 fig, axes = plt.subplots(nrows=2, ncols=1)
 
 for idx, table in enumerate(tables):
@@ -36,37 +73,38 @@ for idx, table in enumerate(tables):
 
     # print(df_resample.head(30))
     # looks like we don't have data from weekends, the stock market is closed
-    # lets interpolate linearly and then take a look
+    # lets regress linearly and then take a look
 
-    df_resample_interpolate = pd.DataFrame().reindex_like(df_resample)
+    df_resample_regression = pd.DataFrame().reindex_like(df_resample)
 
     # only nasdaq has volume column
     if idx == 0:
-        df_resample_interpolate[['price', 'open', 'high', 'low', 'volume_currency']] = \
-            df_resample[['price', 'open', 'high', 'low', 'volume_currency']].interpolate()
+        df_resample_regression[['price', 'open', 'high', 'low', 'volume_currency']] = \
+            df_resample[['price', 'open', 'high', 'low', 'volume_currency']].apply(regression_with_past)
+
     else:
-        df_resample_interpolate[['price', 'open', 'high', 'low']] = \
-            df_resample[['price', 'open', 'high', 'low']].interpolate()
+        df_resample_regression[['price', 'open', 'high', 'low']] = \
+            df_resample[['price', 'open', 'high', 'low']].apply(regression_with_past)
 
     # recalculate the percentage changed in price if non existent
-    df_resample_interpolate['perc_price'] = [(x - y) / x if np.isnan(price) else perc_price
-                                             for x, y, price, perc_price
-                                             in zip(df_resample_interpolate['price'],
-                                                    df_resample_interpolate['price'].shift(1),
-                                                    df_resample['price'],
-                                                    df_resample['perc_price'])]
+    df_resample_regression['perc_price'] = [(x - y) / x if np.isnan(price) else perc_price
+                                            for x, y, price, perc_price
+                                            in zip(df_resample_regression['price'],
+                                                   df_resample_regression['price'].shift(1),
+                                                   df_resample['price'],
+                                                   df_resample['perc_price'])]
 
     if plotit:
         # plot some results
-        df_resample_interpolate.loc['2014-01-01':'2014-01-31', 'price'].plot(marker='.', linestyle='none',
-                                                                             ax=axes[idx], label='interpolated')
+        df_resample_regression.loc['2014-01-01':'2014-01-31', 'price'].plot(marker='.', linestyle='none',
+                                                                            ax=axes[idx], label='regression')
 
         df_resample.loc['2014-01-01':'2014-01-31', 'price'].plot(marker='.', linestyle='none',
                                                                  ax=axes[idx], label='real_data')
 
         axes[idx].set_title('{} price data'.format(table))
 
-    # resend tables as table_interpolate
+    # resend tables as table_regression
     data_types = {"date_": sqlalchemy.types.TIMESTAMP,
                   "price": sqlalchemy.types.FLOAT,
                   "open": sqlalchemy.types.FLOAT,
@@ -75,12 +113,12 @@ for idx, table in enumerate(tables):
                   "volume_currency": sqlalchemy.types.FLOAT,
                   "perc_price": sqlalchemy.types.FLOAT}
 
-    df_resample_interpolate.reset_index(inplace=True)
-    df_resample_interpolate.to_sql('{}_interpolate'.format(table), con=con, if_exists='replace', dtype=data_types,
-                                   index=False)
+    df_resample_regression.reset_index(inplace=True)
+    df_resample_regression.to_sql('{}_regression'.format(table), con=con, if_exists='replace', dtype=data_types,
+                                  index=False)
 
     print(df_resample.head(10))
-    print(df_resample_interpolate.head(10))
+    print(df_resample_regression.head(10))
 
 if plotit:
     # get legend right
